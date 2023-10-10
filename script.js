6cb0a6734616b6542d590fd17a25e97a51a03c32
AbhishekNaik1112 <142667674+AbhishekNaik1112@users.noreply.github.com>
2023-10-10 10:54:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ let gameInterval;
 
 //random food gen
 function generateRandomFoodPosition() {
-    foodX = Math.floor(Math.random() * gridWidth);
-    foodY = Math.floor(Math.random() * gridHeight);
+  foodX = Math.floor(Math.random() * gridWidth);
+  foodY = Math.floor(Math.random() * gridHeight);
 }
 
 //start food position
@@ -35,67 +35,66 @@ generateRandomFoodPosition();
 foodElement.style.left = foodX * cellSize + "px";
 foodElement.style.top = foodY * cellSize + "px";
 
-console.log(foodX)
-console.log(foodY)
+console.log(foodX);
+console.log(foodY);
 // console.log(foodElement)
 
 // Directional update
 function updateGame() {
-    if (snakeDirection === "right") {
-        snakeX++;
-    } else if (snakeDirection === "left") {
-        snakeX--;
-    } else if (snakeDirection === "up") {
-        snakeY--;
-    } else if (snakeDirection === "down") {
-        snakeY++;
-    }
+  if (snakeDirection === "right") {
+    snakeX++;
+  } else if (snakeDirection === "left") {
+    snakeX--;
+  } else if (snakeDirection === "up") {
+    snakeY--;
+  } else if (snakeDirection === "down") {
+    snakeY++;
+  }
 
-    // eats food?
-    if (snakeX == foodX && snakeY == foodY) {
-        // Increase score
-        score++;
-        // speed+=300;
-        speed -= 5;
-        scoreCard.textContent = score;
-        console.log(score)
-        // console.log(scoreCard)
-        console.log(speed)
-        clearInterval(gameInterval);
-        gameInterval = setInterval(updateGame, speed);
+  // eats food?
+  if (snakeX == foodX && snakeY == foodY) {
+    // Increase score
+    score+=1;
+    // speed+=300;
+    speed -= 5;
+    scoreCard.textContent = score;
+    console.log(score);
+    // console.log(scoreCard)
+    console.log(speed);
+    clearInterval(gameInterval);
+    gameInterval = setInterval(updateGame, speed);
 
-        // snake == eat food? => new food
-        generateRandomFoodPosition();
-        foodElement.style.left = foodX * cellSize + "px";
-        foodElement.style.top = foodY * cellSize + "px";
-    }
-    console.log(foodX)
-console.log(foodY)
+    // snake == eat food? => new food
+    generateRandomFoodPosition();
+    foodElement.style.left = foodX * cellSize + "px";
+    foodElement.style.top = foodY * cellSize + "px";
+  }
+  console.log(foodX);
+  console.log(foodY);
 
-    // Wall crashes
-    if (snakeX < 0 || snakeX >= gridWidth || snakeY < 0 || snakeY >= gridHeight) {
-        clearInterval(gameInterval);
-        swal("Game over! Your score is: " + score);
-        return;
-    }
+  // Wall crashes
+  if (snakeX < 0 || snakeX >= gridWidth || snakeY < 0 || snakeY >= gridHeight) {
+    clearInterval(gameInterval);
+    swal("Game over! Your score is: " + score);
+    return;
+  }
 
-    // Update snake position
-    snakeElement.style.left = snakeX * cellSize + "px";
-    snakeElement.style.top = snakeY * cellSize + "px";
+  // Update snake position
+  snakeElement.style.left = snakeX * cellSize + "px";
+  snakeElement.style.top = snakeY * cellSize + "px";
 }
 
-
-// Arrow key 
+// Arrow key
 document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowRight" && snakeDirection !== "left") {
-        snakeDirection = "right";
-    } else if (event.key === "ArrowLeft" && snakeDirection !== "right") {
-        snakeDirection = "left";
-    } else if (event.key === "ArrowUp" && snakeDirection !== "down") {
-        snakeDirection = "up";
-    } else if (event.key === "ArrowDown" && snakeDirection !== "up") {
-        snakeDirection = "down";
-    }
+  if (event.key === "ArrowRight" && snakeDirection !== "left") {
+    snakeDirection = "right";
+  } else if (event.key === "ArrowLeft" && snakeDirection !== "right") {
+    snakeDirection = "left";
+  } else if (event.key === "ArrowUp" && snakeDirection !== "down") {
+    snakeDirection = "up";
+  } else if (event.key === "ArrowDown" && snakeDirection !== "up") {
+    snakeDirection = "down";
+  }
 });
 // console.log(snakeX)
 // console.log(snakeY)
@@ -104,28 +103,28 @@ console.log(snakeDirection);
 
 // Play btn
 playButton.addEventListener("click", () => {
-    if (!gameInterval) {
-        gameInterval = setInterval(updateGame, 200);
-    }
+  if (!gameInterval) {
+    gameInterval = setInterval(updateGame, 200);
+  }
 });
 
 //Reset btn
 resetButton.addEventListener("click", () => {
-    clearInterval(gameInterval);
-    //   console.log(gameInterval);
-    snakeX = 10;
-    snakeY = 10;
-    snakeDirection = "right";
-    score = 0;
-    scoreCard.textContent = score;
-    //   console.log(gameInterval);
+  clearInterval(gameInterval);
+  //   console.log(gameInterval);
+  snakeX = 10;
+  snakeY = 10;
+  snakeDirection = "right";
+  score = 0;
+  scoreCard.textContent = score;
+  //   console.log(gameInterval);
 
-    gameInterval = null;
-    // console.log(gameInterval);
+  gameInterval = null;
+  // console.log(gameInterval);
 
-    //Reset snake posi
-    snakeElement.style.left = snakeX * cellSize + "px";
-    snakeElement.style.top = snakeY * cellSize + "px";
+  //Reset snake posi
+  snakeElement.style.left = snakeX * cellSize + "px";
+  snakeElement.style.top = snakeY * cellSize + "px";
 });
 
 //reset game
